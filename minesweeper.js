2041c38606c495d64a449d29d030a8b98cd1cc00
Joao Novis <novis.joao@gmail.com>
2023-11-07 08:58:36 +0100

    a third attempt

--- minesweeper.js
+++ minesweeper.js
@@ -34,7 +34,7 @@ const chunkify = (array, chunk) => {
   return result;
 };
 
-export const makeArray = (cols, rows, bombs) => {
+const makeArray = (cols, rows, bombs) => {
   const total = cols * rows;
   const noBobs = total - bombs;
   const grid = [];
@@ -48,13 +48,13 @@ export const makeArray = (cols, rows, bombs) => {
   return chunkify(shuffled, cols);
 };
 
-export const getLoc = (tile) => {
+const getLoc = (tile) => {
   const x = tile.cellIndex;
   const y = tile.parentElement.rowIndex;
   return { x, y };
 };
 
-export const genGrid = (grid, gameZone) => {
+const genGrid = (grid, gameZone) => {
   grid.forEach((row) => {
     const tr = document.createElement("tr");
     row.forEach((cell) => {
@@ -70,7 +70,7 @@ export const genGrid = (grid, gameZone) => {
   });
 };
 
-export const scanNeighbours = ({ x, y }, maxX, maxY, ref) => {
+const scanNeighbours = ({ x, y }, maxX, maxY, ref) => {
   const arr = [
     x - 1 >= 0 && y - 1 >= 0 ? [x - 1, y - 1] : [x, y],
     y - 1 >= 0 ? [x, y - 1] : [x, y],
@@ -135,7 +135,7 @@ const noBombsNear = (target, array, grid) => {
   });
 };
 
-export const uncover = (target, grid) => {
+const uncover = (target, grid) => {
   if (target.dataset.bomb === "true") {
     target.classList.add("mine");
     return true;
@@ -146,14 +146,124 @@ export const uncover = (target, grid) => {
   }
 };
 
-export const doubleClick = (target, grid) => {
+const doubleClick = (target, grid) => {
   if (!target.classList.contains("flagged")) {
     const lost = uncoverAllNeighbours(target, grid);
     return lost;
   }
 };
 
-export const checkIfWon = (numBombs) => {
+const checkIfWon = (numBombs) => {
   const flaggedOrUnopened = document.querySelectorAll(".flagged,.unopened");
   return flaggedOrUnopened.length === numBombs;
 };
+
+const explosionSound = new Audio("/sounds/exp.mp3");
+
+const gameZone = document.getElementById("minesweeper");
+const bombCounter = document.getElementById("bomb-count");
+const diffSelector = document.getElementById("dif");
+const timer = document.getElementById("timer");
+
+let seconds = 0;
+let gameOver = false;
+
+const pickDifficulty = (e) => {
+  localStorage.setItem("dificulty", diffSelector.value);
+  location.reload();
+};
+
+const timerFunc = () => {
+  if (!gameOver) {
+    seconds += 1;
+    if (seconds <= 9) {
+      timer.innerText = `0${seconds}`;
+    } else {
+      timer.innerText = seconds;
+    }
+  }
+};
+
+const isGameWon = (numBombs, isBomb) => {
+  const condition = document.getElementById("condition");
+  if (isBomb) {
+    explosionSound.play();
+    gameOver = true;
+    condition.innerText = "You Lost 😢";
+  } else if (checkIfWon(numBombs)) {
+    gameOver = true;
+    condition.innerText = "You Won!";
+  }
+};
+
+document.addEventListener(
+  "click",
+  () => {
+    setInterval(timerFunc, 1000);
+  },
+  { once: true }
+);
+
+const diff = localStorage.getItem("dificulty");
+
+let rows = 30;
+let cols = 16;
+let numBombs = 99;
+
+if (diff === "3") {
+  rows = 30;
+  cols = 16;
+  numBombs = 99;
+  diffSelector.value = "3";
+} else if (diff === "2") {
+  rows = 16;
+  cols = 16;
+  numBombs = 40;
+  diffSelector.value = "2";
+} else {
+  rows = 9;
+  cols = 9;
+  numBombs = 10;
+  diffSelector.value = "1";
+}
+bombCounter.innerText = numBombs;
+const grid = makeArray(cols, rows, numBombs);
+
+diffSelector.addEventListener("change", pickDifficulty);
+
+genGrid(grid, gameZone);
+
+const plantFlag = (e) => {
+  e.preventDefault();
+  if (!gameOver) {
+    const target = e.target;
+    if (target.classList.contains("unopened") && !target.classList.contains("flagged")) {
+      target.classList.add("flagged");
+    } else {
+      target.classList.remove("flagged");
+    }
+    const numFlagged = document.querySelectorAll(".flagged").length;
+    bombCounter.innerText = numBombs - numFlagged;
+    isGameWon(numBombs);
+  }
+};
+
+const dig = (e) => {
+  if (!gameOver) {
+    const target = e.target;
+    let isBomb = false;
+    if (!target.classList.contains("flagged")) {
+      isBomb = uncover(target, grid);
+    }
+    isGameWon(numBombs, isBomb);
+  }
+};
+
+const double = (e) => {
+  const isBomb = doubleClick(e.target, grid);
+  isGameWon(numBombs, isBomb);
+};
+
+gameZone.addEventListener("click", dig);
+gameZone.addEventListener("contextmenu", plantFlag);
+gameZone.addEventListener("dblclick", double);
